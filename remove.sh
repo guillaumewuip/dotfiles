7681691e4c36e1b0b64646eabd7dcf1559fb15bf
Guillaume Wuip <guillaume@wuips.com>
2018-02-18 22:26:44 +0100

    Fix remove.sh

--- remove.sh
+++ remove.sh
@@ -4,20 +4,14 @@ HOME=~/          #home dir
 
 find -E . -mindepth 1 -maxdepth 1 -a ! -regex "./($IGNORE_FILES)" | while read i
 do
-    i=`basename $i`
+  i=`basename $i`
 
-    if [ $i == "install.sh" -o $i == "remove.sh" -o "README.md"
-        -o ".git" -o ".gitignore"]; then
-        continue;
-    fi
+  file=`pwd`/$i
+  homeFile=$HOME$i
 
-    file=`pwd`/$i
-    homeFile=$HOME$i
-
-    if [ -h $homeFile -a `readlink $homeFile` == $file ]; then
-        echo "unlink $homeFile"
-        unlink $homeFile
-    fi
-
+  if [ -h $homeFile ] && [ `readlink $homeFile` == $file ]; then
+    echo "unlink $homeFile"
+    unlink $homeFile
+  fi
 done
 
