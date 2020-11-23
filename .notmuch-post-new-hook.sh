ed55eda3021bc81eec1dfa71b47ca763f86e4374
Guillaume Wuip <guillaume@wuips.com>
2020-11-23 21:38:36 +0100

    add mail

--- .notmuch-post-new-hook.sh
+++ .notmuch-post-new-hook.sh
@@ -15,8 +15,9 @@ notmuch tag -inbox +draft -- path:/.*Brouillons*./
 notmuch tag +guigui-wuip-gmail -- path:guigui-wuip-gmail/**
 notmuch tag +clochard-guillaume-gmail -- path:clochard-guillaume-gmail/**
 notmuch tag +guillaume-wuips -- path:guillaume-wuips/**
+notmuch tag +guillaume-clochard-iadvize -- path:guillaume-clochard-iadvize-gmail/**
 
 # tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -inbox +sent +archive -- 'not tag:draft and (from:guillaume@wuips.com or from:clochard.guillaume@gmail.com or from:guigui.wuip@gmail.com)'
+notmuch tag -inbox +sent +archive -- 'not tag:draft and (from:guillaume@wuips.com or from:clochard.guillaume@gmail.com or from:guigui.wuip@gmail.com or from:guillaume.clochard@iadvize.com)'
 
 echo "post-new completed"
