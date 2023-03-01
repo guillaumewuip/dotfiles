4a5b9d4499fed428fa7950d5a2ad3da3f2a2165e
Guillaume Clochard <guillaume.clochard@blablacar.com>
2023-03-01 14:57:37 +0100

    update vim

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -30,7 +30,34 @@ vim.diagnostic.config({
 vim.keymap.set('n', '<leader>ap', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>an', vim.diagnostic.goto_next)
 
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+local hover = function()
+  -- vim.diagnostic.open_float(nil, { scope = "cursor", focus = false })
+  local float_opts = {
+    focusable = false,
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    border = "rounded",
+    source = "always", -- show source in diagnostic popup window
+    prefix = " ",
+  }
+
+  if not vim.b.diagnostics_pos then
+    vim.b.diagnostics_pos = { nil, nil }
+  end
+
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  if (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2]) and #vim.diagnostic.get() > 0
+  then
+    vim.diagnostic.open_float(nil, float_opts)
+  end
+
+  vim.b.diagnostics_pos = cursor_pos
+end
+
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  callback = hover,
+})
 
 use {
   "b0o/schemastore.nvim",
@@ -137,7 +164,6 @@ use {
           vim.keymap.set('', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('', 'gi', vim.lsp.buf.implementation, opts)
 
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
           vim.keymap.set('', '<leader>rn', vim.lsp.buf.rename, opts)
