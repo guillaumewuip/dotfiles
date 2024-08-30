819cc143051f79ce29984a0b0fce0c3f9511ad12
Guillaume Clochard <guillaume.clochard@blablacar.com>
2024-08-30 22:52:35 +0200

    update vim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -556,6 +556,20 @@ return {
   {
     "nvim-pack/nvim-spectre",
     keys = { "<leader>g" },
+    config = function()
+      require("spectre").setup {
+        replace_engine = {
+          ["sed"] = {
+            cmd = "sed",
+            args = {
+              "-i",
+              "",
+              "-E",
+            },
+          },
+        },
+      }
+    end,
   },
 
   {
