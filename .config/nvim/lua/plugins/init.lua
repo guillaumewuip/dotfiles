c5c0a027032d48c79fd1834fdde56398a1c52dd9
Guillaume Clochard <guillaume.clochard@blablacar.com>
2024-09-22 10:59:53 +0200

    update vim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -566,58 +566,6 @@ return {
     end
   },
 
-  -- {
-  --   "nvim-pack/nvim-spectre",
-  --   keys = { "<leader>g" },
-  --   config = function()
-  --     require("spectre").setup {
-  --       find_engine = {
-  --         -- rg is map with finder_cmd
-  --         ["rg"] = {
-  --           cmd = "rg",
-  --           -- default args
-  --           args = {
-  --             "--color=never",
-  --             "--no-heading",
-  --             "--with-filename",
-  --             "--line-number",
-  --             "--hidden",
-  --             "--column",
-  --             "--glob",
-  --             "!**/.git/*",
-  --             "--glob",
-  --             "!**/node_modules/*",
-  --           },
-  --           options = {
-  --             ["ignore-case"] = {
-  --               value = "--ignore-case",
-  --               icon = "[I]",
-  --               desc = "ignore case",
-  --             },
-  --             ["hidden"] = {
-  --               value = "--hidden",
-  --               desc = "hidden file",
-  --               icon = "[H]",
-  --             },
-  --             -- you can put any rg search option you want here it can toggle with
-  --             -- show_option function
-  --           },
-  --         },
-  --       },
-  --       replace_engine = {
-  --         ["sed"] = {
-  --           cmd = "sed",
-  --           args = {
-  --             "-i",
-  --             "",
-  --             "-E",
-  --           },
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
-
   {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
