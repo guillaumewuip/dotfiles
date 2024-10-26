095b5a6b019b82665d42f3f85bd124c09e44ffe4
Guillaume Clochard <guillaume.clochard@blablacar.com>
2024-10-26 09:41:31 +0200

    update vim theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,9 +91,10 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+-- load nvui theme cache files
+for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+  dofile(vim.g.base46_cache .. v)
+end
 
 require "nvchad.autocmds"
 
--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,43 +1,44 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.base46 = {
+  theme = "onedark",
+  theme_toggle = {},
+  transparency = true,
+
+  hl_override = {
+    IlluminatedWordText = {},
+    IlluminatedWordRead = {},
+    IlluminatedWordWrite = {},
+    Folded = {
+      bg = "#353b45",
+    },
+  },
+
+  hl_add = {},
+
+}
+
 M.ui = {
-	theme = "onedark",
-	theme_toggle = {},
-	transparency = true,
-	lsp_semantic_tokens = true,
+  lsp_semantic_tokens = true,
 
-	hl_override = {
-		-- Comment = {
-		--   italic = true,
-		-- },
-		IlluminatedWordText = {},
-		IlluminatedWordRead = {},
-		IlluminatedWordWrite = {},
-		Folded = {
-			bg = "#353b45",
-		},
-	},
+  statusline = {
+    enabled = false,
+  },
 
-	hl_add = {},
+  tabufline = {
+    enabled = false,
+  },
 
-	statusline = {
-		enabled = false,
-	},
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default",           -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg",    -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "simple", -- colored / simple
+  },
 
-	tabufline = {
-		enabled = false,
-	},
-
-	cmp = {
-		icons = true,
-		lspkind_text = true,
-		style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-		border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-		selected_item_bg = "simple", -- colored / simple
-	},
-
-	telescope = { style = "borderless" }, -- borderless / bordered
+  telescope = { style = "borderless" }, -- borderless / bordered
 }
 
 return M
