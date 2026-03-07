8913cc6812917dea9a746226ee5f4d7bd44d76d6
Guillaume Clochard <guillaume.clochard@blablacar.com>
2026-03-07 23:21:17 +0100

    alias

--- .zshrc.d/2.alias.zsh
+++ .zshrc.d/2.alias.zsh
@@ -45,6 +45,6 @@ alias p="pnpm turbo run build"
 alias ci="pnpm run format --fix && pnpm run lint --fix && pnpm turbo --ui=stream run typings && pnpm turbo --ui=stream run test:unit && pnpm --ui=stream turbo run build"
 
 alias c="copilot --enable-all-github-mcp-tools --allow-all-tools --experimental --disable-mcp-server=tana --disable-mcp-server=atlassian --model=claude-sonnet-4.6"
-alias t="copilot --allow-all-tools --disable-mcp-server=atlassian --disable-mcp-server=context7 --model=claude-opus-4.6"
+alias t="copilot --allow-all-tools --disable-mcp-server=atlassian --disable-mcp-server=context7 --model=gpt-5.4"
 
 alias n='vim __notes.md'
