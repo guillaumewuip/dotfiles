623f33939173c0a783e50fc6a853c1824741deca
Guillaume Clochard <guillaume.clochard@blablacar.com>
2026-04-27 17:33:27 +0200

    update mcp

--- .zshrc.d/2.alias.zsh
+++ .zshrc.d/2.alias.zsh
@@ -44,7 +44,7 @@ alias pp="pnpm i; pnpm turbo run build"
 alias p="pnpm turbo run build"
 alias ci="pnpm run format --fix && pnpm run lint --fix && pnpm turbo run typings --ui=stream && pnpm turbo run test:unit --ui=stream && pnpm turbo run build --ui=stream"
 
-alias c="copilot --enable-all-github-mcp-tools --allow-all-tools --experimental --disable-mcp-server=tana --disable-mcp-server=atlassian --model=claude-sonnet-4.6"
+alias c="copilot --enable-all-github-mcp-tools --allow-all-tools --experimental --disable-mcp-server=tana --disable-mcp-server=atlassian --disable-mcp-server=annie --disable-mcp-server=exa--model=claude-sonnet-4.6"
 alias t="copilot --allow-all-tools --disable-mcp-server=atlassian --disable-mcp-server=context7 --model=claude-sonnet-4.6 --effort=high --yolo"
 
 alias n='vim __notes.md'
